5875e0ff7488404ceb8279b26990482fc3c58157
thermius <156609541+thermius@users.noreply.github.com>
2026-08-25 17:43:18 -0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,11 @@
+import argumentos_dir.ArgumentosVariados; 
+
+public class Argumentos
+{
+	public static void main ()
+	{
+		ArgumentosVariados exemplo = new ArgumentosVariados();		
+		exemplo.ExibirLista("ola","mundo","55","?");
+	}
+}	
+
